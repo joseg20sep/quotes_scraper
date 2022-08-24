5e74e9fb0c374f4b25ab9c6ca53dfc51fe3f98d4
Jose Medina <jgmsxsimpre@gmail.com>
2022-08-24 05:43:30 -0500

    finalizar scrapy

--- quotes_scraper/quotes_scraper/spiders/quotes.py
+++ quotes_scraper/quotes_scraper/spiders/quotes.py
@@ -1,12 +1,14 @@
 from email.quoprimime import quote
+from pickle import TRUE
 from turtle import title
 
 import scrapy
 #titulo = response.xpath('//div/h1/a/text()').get()
-#top_ten_tag = response.xpath('//div[contains(@class,"tags-box")]//span[@class="tag-item"]/a/text()').getall()
+#top_tag = response.xpath('//div[contains(@class,"tags-box")]//span[@class="tag-item"]/a/text()').getall()
 #citas= response.xpath('//span[@class="text" and @itemprop="text"]/text()').getall()
 #guardar los archivos en el formato deseado--> scrapy crawl quotes -o quotes.csv
 #button --> response.xpath('//ul[@class = "pager"]//li[@class="next"]/a/@href').get()
+# guardar solo el top que quieras --> scrapy crawl quotes -a top=5
 class QuotesSpider(scrapy.Spider):
       # name es el nombre unico con el scrapy se va referir al spider dentro del proyect.
     # name debe ser unico.
@@ -19,7 +21,14 @@ class QuotesSpider(scrapy.Spider):
     #}
     custom_settings = {
         'FEED_URI': 'quotes.json',
-        'FEED_FORMAT': 'json'
+        #Aqui eliges el formato en que se quiere guardar el archivo
+        'FEED_FORMAT': 'json',
+        'FEED_EXPORT_ENCODING': 'utf-8',
+        'CONCURRENT_REQUESTS': 24,
+        'MEMUSAGE_LIMIT_MB': 2048,
+        'MEMUSAGE_NOTIFY_MAIL': ['joseg20sep@gmail.com'],
+        'ROBOTSTXT_OBEY': True,
+        'USER_AGENT': 'Joseg20sep'
     }
     def parse_only_quotes(self, response, **kwargs):
         if kwargs:
@@ -48,16 +57,20 @@ class QuotesSpider(scrapy.Spider):
         #for quotes in quotes:
         #    print(f'- {quotes}')
         #print('\n\n')
-        top_ten_tag = response.xpath('//div[contains(@class,"tags-box")]//span[@class="tag-item"]/a/text()').getall()
+        top_tag = response.xpath('//div[contains(@class,"tags-box")]//span[@class="tag-item"]/a/text()').getall()
+        top = getattr(self, 'top', None)
+        if top:
+            top = int(top)
+            top_tag = top_tag[:top]
        #print('Top ten tag: ')
-       #for tag in top_ten_tag:
+       #for tag in top_tag:
        #    print(f'- {tag}')
         #print('\n\n')
         #print('\n\n')
         #print('*' * 10)
         yield {
             'title': title,
-            'top_ten_tag': top_ten_tag
+            'top_tag': top_tag
         }
         next_page_button_link = response.xpath('//ul[@class = "pager"]//li[@class="next"]/a/@href').get()
         if next_page_button_link:
